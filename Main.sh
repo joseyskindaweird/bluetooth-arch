554639f57bd555b0df42edc4f66f67cd5a8c1374
Strange <gomezjoseph2411@gmail.com>
2024-02-11 16:37:16 -0600

    Create Main.sh
    Idiot

---
+++ Main.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Running from curl.
+clear
+echo "How's it going? We're gonna get you ready..."
+
+echo "Installing.. be patient.."
+sudo pacman -S bluez blueman bluez-utils --noconfirm
+
+# Probing >:3
+clear
+echo "Probing.."
+sudo modprobe btusb
+
+# Starting services
+clear
+echo "Starting Bluetooth service.."
+sudo systemctl enable bluetooth && sudo systemctl start bluetooth
+
+# DOIT!!!!!!!
+clear
+read -p "Do you want to reboot now to apply changes? (Y/N)? " reboot_choice
+if [[ $reboot_choice == "Y" || $reboot_choice == "y" ]]; then
+    clear
+    echo "Bye!!!!!!!"
+    sudo reboot
+fi
