4858ba40240b4d2928648d2d72b66608a8949cf7
Strange <84677793+strangetheproot@users.noreply.github.com>
2024-02-12 16:34:13 -0600

    Update Main.sh
    Added some small stuff (this better not break)

--- Main.sh
+++ Main.sh
@@ -1,27 +1,30 @@
 #!/bin/bash
 
-# Running from curl.
+# Beginning of the script
 clear
-echo "How's it going? We're gonna get you ready..."
-
+echo "Starting.."
 echo "Installing.. be patient.."
 sudo pacman -S bluez blueman bluez-utils --noconfirm
+echo "Finished!"
 
-# Probing >:3
+# Probing for the btusb service
 clear
 echo "Probing.."
 sudo modprobe btusb
+echo "Probed!"
 
 # Starting services
 clear
 echo "Starting Bluetooth service.."
 sudo systemctl enable bluetooth && sudo systemctl start bluetooth
+echo "Service started.."
 
-# DOIT!!!!!!!
+# Reboot or nah?
 clear
 read -p "Do you want to reboot now to apply changes? Yup, we're already done :3 (Y/N)? " reboot_choice
 if [[ $reboot_choice == "Y" || $reboot_choice == "y" ]]; then
     clear
     echo "Bye!!!!!!!"
+    echo "Made by strange"
     sudo reboot
 fi
